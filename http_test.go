be4e0bddee7d4d183d9604269ca7940e3f4f1ced
Asim Aslam <asim@aslam.me>
2019-06-21 15:24:15 +0100

    Move selector to client/selector

--- http_test.go
+++ http_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-micro/registry/memory"
-	"github.com/micro/go-micro/selector"
+	"github.com/micro/go-micro/client/selector"
 	"github.com/micro/go-plugins/client/http/test"
 )
 
