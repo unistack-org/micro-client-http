18eb0d9e5ce695b5136dd7e8088be49482766cb2
Vasiliy Tolstov <v.tolstov@unistack.org>
2023-04-28 22:29:18 +0300

    move to micro v4
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- util.go
+++ util.go
@@ -10,10 +10,10 @@ import (
 	"strings"
 	"sync"
 
-	"go.unistack.org/micro/v3/client"
-	"go.unistack.org/micro/v3/errors"
-	"go.unistack.org/micro/v3/logger"
-	rutil "go.unistack.org/micro/v3/util/reflect"
+	"go.unistack.org/micro/v4/client"
+	"go.unistack.org/micro/v4/errors"
+	"go.unistack.org/micro/v4/logger"
+	rutil "go.unistack.org/micro/v4/util/reflect"
 )
 
 var (
