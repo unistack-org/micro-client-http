0ed184fda086b8fe53f9d0609b9c69ff1bbe0d7a
pugnack <pugnack@gmail.com>
2025-09-25 03:30:54 +0500

    improve unit-tests

--- client_unary_call_test.go
+++ client_unary_call_test.go
@@ -802,6 +802,7 @@ func TestClient_Call_ErrorsMap(t *testing.T) {
 				opts...,
 			)
 
+			require.NoError(t, err)
 			s, ok := status.FromError(err)
 			require.True(t, ok)
 			require.NotNil(t, s)
